f0f4c68712051af931231788d012eb3b3e22afec
nilpatel <nil.p@simformsolutions.com>
2023-02-27 16:02:59 +0530

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Student.create(name: "Nil Patel",enroll: "19028011")
+Student.create(name: "Aditya Kansara",enroll: "190280112")
+Student.create(name: "Amam Vhora",enroll: "190280113")
+Student.create(name: "Krutik Patel",enroll: "190280114")
+Student.create(name: "Dhey Sapra",enroll: "190280115")
+
